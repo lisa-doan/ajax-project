bbc222793bde792a82ee9d709a757fca0f93c58c
lisadoann <ldoan24@gmail.com>
2021-02-23 20:39:47 +0000

    Added xmlhttprequest to get API

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function getWord(word) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.dictionaryapi.dev/api/v2/entries/en_US/' + word);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    console.log(xhr.response);
+  });
+  xhr.send();
+}
