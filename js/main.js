82a6e99ede290f0674458deff67fdf7c7dcdbfc3
lisadoann <ldoan24@gmail.com>
2021-02-24 19:31:05 +0000

    Added XMLHttpRequest for a random word

--- js/main.js
+++ js/main.js
@@ -9,6 +9,26 @@ function getWord(word) {
   xhr.send();
 }
 
+function getWord(word) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.dictionaryapi.dev/api/v2/entries/en_US/' + word);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log(xhr.status);
+    console.log(xhr.response);
+  });
+  xhr.send();
+}
+
+var xhrRandomWord = new XMLHttpRequest();
+xhrRandomWord.open('GET', 'https://random-word-api.herokuapp.com//word?number=1');
+xhrRandomWord.responseType = 'json';
+xhrRandomWord.addEventListener('load', function () {
+  console.log('xhrRandomWord.status: ', xhrRandomWord.status);
+  console.log('xhrRandomWord.response: ', xhrRandomWord.response);
+});
+xhrRandomWord.send();
+
 var $homelink = document.querySelector('.home-link');
 var $homePage = document.querySelector('.homepage-container');
 var $wordPage = document.querySelector('.wotd-container');
