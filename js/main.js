4cf9404ba82ca0f962fe433313b22f569f9752b8
lisadoann <ldoan24@gmail.com>
2021-02-24 18:38:59 +0000

    Added eventlistener, click, to view homepage

--- js/main.js
+++ js/main.js
@@ -6,6 +6,19 @@ function getWord(word) {
     console.log(xhr.status);
     console.log(xhr.response);
   });
-
   xhr.send();
 }
+
+var $homelink = document.querySelector('.home-link');
+var $homePage = document.querySelector('.homepage-container');
+var $wordPage = document.querySelector('.wod-container');
+
+function viewHomepage(event) {
+  console.log('ok this works');
+  $homePage.className = 'homepage-container display';
+  $wordPage.className = 'wod-container hidden';
+
+}
+
+$homelink.addEventListener('click', viewHomepage)
+;
