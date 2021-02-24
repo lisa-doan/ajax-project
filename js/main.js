ac5b0522d3944e41f8b8d8d40f5b6e7be386b00e
lisadoann <ldoan24@gmail.com>
2021-02-24 22:55:36 +0000

    Called getRandomWord and removed unneccessay code

--- js/main.js
+++ js/main.js
@@ -1,18 +1,11 @@
-var xhrRandomWord = new XMLHttpRequest();
-xhrRandomWord.open('GET', 'https://random-word-api.herokuapp.com/word?number=1');
-xhrRandomWord.responseType = 'json';
-xhrRandomWord.addEventListener('load', function () {
-  var $wordOfTheDay = document.querySelector('.wotd');
-  $wordOfTheDay.textContent = xhrRandomWord.response[0];
-});
-xhrRandomWord.send();
-
 var $homelink = document.querySelector('.home-link');
 var $homePage = document.querySelector('.homepage-container');
 var $wordPage = document.querySelector('.wotd-container');
 var $navBar = document.querySelector('.nav-bar-container');
 var $wotdButton = document.querySelector('.wotd-button');
 var $newWordButton = document.querySelector('.new-word');
+
+getRandomWord();
 
 function getRandomWord(event) {
   var xhrRandomWord = new XMLHttpRequest();
