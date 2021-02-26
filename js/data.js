afc96fa9b952e5e64c3fd577d3bd1d1557d23b55
lisadoann <ldoan24@gmail.com>
2021-02-26 19:47:23 +0000

    Changed obj property names

--- js/data.js
+++ js/data.js
@@ -1,8 +1,8 @@
 /* exported data */
 var data = {
   view: 'wordlist-view',
-  entries: [],
-  nextId: 1
+  words: [],
+  nextIdNum: 1
 };
 
 var previousSavedJSON = localStorage.getItem('saved-local-storage');
