5d36550622b5862e365d986e39b1237aba878550
lisadoann <ldoan24@gmail.com>
2021-02-26 15:56:07 +0000

    Added local storage when user click save

--- js/data.js
+++ js/data.js
@@ -1,1 +1,18 @@
 /* exported data */
+var data = {
+  view: 'wordlist-view',
+  entries: [],
+  nextId: 1
+};
+
+var previousSavedJSON = localStorage.getItem('saved-local-storage');
+
+if (previousSavedJSON !== null) {
+  data = JSON.parse(previousSavedJSON);
+
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var todosJSON = JSON.stringify(data);
+  localStorage.setItem('saved-local-storage', todosJSON);
+});
